d280857ecb7b64d586d2285c098bf21047ccc7b1
SubirSam <saptarshi.subir@gmail.com>
2025-01-04 17:18:35 +0530

    updated enrty point

--- MultiProcessorInitial/src/main/java/org/multiprocess/Main.java
+++ MultiProcessorInitial/src/main/java/org/multiprocess/Main.java
@@ -3,5 +3,6 @@ package org.multiprocess;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("today we are going for multiprocessing in java!!!!");
     }
 }
